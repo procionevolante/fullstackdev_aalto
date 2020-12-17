94e99ceec5557c1478da53e962f53aa0eef6dbeb
Andrea Fanti <andrea.fanti@aalto.fi>
2020-12-17 19:19:51 +0200

    1.14 done. part 1 completed

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,16 +3,33 @@ import ReactDOM from 'react-dom'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
+  const [anecdotes, setAnecdotes] = useState(props.anecdotes.map( a => ({
+    str : a,
+    votes : 0
+  })));
+
+  const voteUp = () => {
+    const newAnecs = [...anecdotes];
+    newAnecs[selected].votes++;
+    setAnecdotes(newAnecs);
+  }
 
   return <>
-    <div>
-      {props.anecdotes[selected]}
-    </div>
-    <button onClick={() => setSelected(Math.floor(Math.random() * props.anecdotes.length))}>
+    <h1>Anecdote of the day</h1>
+    <Anecdote anec = {anecdotes[selected]} />
+    <button onClick={voteUp}>vote</button>
+    <button onClick={() => setSelected(Math.floor(Math.random() * anecdotes.length))}>
       next anecdote
     </button>
+    <h1>Anecdote with most votes</h1>
+    <Anecdote anec = {anecdotes.reduce((acc, cur) => (cur.votes > acc.votes? cur:acc))} />
   </>
 }
+
+const Anecdote = ({anec}) => (<>
+  <div>{anec.str}</div>
+  <div>has {anec.votes} votes</div>
+</>)
 
 const anecdotes = [
   'If it hurts, do it more often',
@@ -20,7 +37,7 @@ const anecdotes = [
   'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
   'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
   'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
 ]
 
 ReactDOM.render(
