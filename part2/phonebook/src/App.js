2205f2621be485fe0d1875bec3b3c9519e50cd39
Andrea Fanti <andrea.fanti@aalto.fi>
2020-12-26 14:09:33 +0200

    ex. 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,24 +1,43 @@
 import React, { useState } from 'react';
+import Person from './components/Person';
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
+    { id:1, name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+
+  // triggered on form submit
+  const addPersonToPhonebook = (event) => {
+    event.preventDefault();
+    // newId = maxID + 1
+    const newId = persons.reduce((acc, cur) => ((cur > acc)? cur : acc), 0) + 1;
+
+    setPersons(persons.concat({
+      id : newId,
+      name: newName
+    }));
+  }
+  const handleNameChange = (event) => {
+    console.log(event.target.value);
+    setNewName(event.target.value);
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPersonToPhonebook}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {
+        persons.map(p => <Person key={p.id} person={p} />)
+      }
     </div>
   )
 }
