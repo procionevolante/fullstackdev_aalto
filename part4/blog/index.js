a71e9f07e17193de0fa2e750fd59b58be89b96b7
Andrea Fanti <andrea.fanti@aalto.fi>
2021-01-06 01:29:44 +0200

    ex. 4.2

--- part4/blog/index.js
+++ part4/blog/index.js
@@ -2,6 +2,9 @@ const app = require('./app');
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 
+// praise the hypnotoad!
+// AKA 'small change to make a meaningful commit msg on git without learning how to rename old ones'
+
 app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`)
+  logger.info(`Server running on port ${config.PORT}`);
 })
