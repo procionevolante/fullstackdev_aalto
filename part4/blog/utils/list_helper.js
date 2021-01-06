2c821f472dd5b9fe28d39719e11c272fe73d8fd7
Andrea Fanti <andrea.fanti@aalto.fi>
2021-01-06 13:26:55 +0200

    ex. 4.4

--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,11 @@ const dummy = (blogs) => (
   1
 )
 
+const totalLikes = (blogs) => (
+  blogs.map(b => b.likes).reduce((acc, cur) => cur + acc)
+)
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
