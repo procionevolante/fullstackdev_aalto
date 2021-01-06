3e0a81754def34451001b0b4feece331939539eb
Andrea Fanti <andrea.fanti@aalto.fi>
2021-01-06 19:01:08 +0200

    ex. 4.19

--- part4/blog/utils/middleware.js
+++ part4/blog/utils/middleware.js
@@ -12,7 +12,7 @@ const errorHandler = (error, req, res, next) => {
 	else if (error.name === 'ValidationError')
 		return res.status(400).json({error: error.message});
   else if (error.name === 'JsonWebTokenError')
-    return response.status(401).json({error:'invalid token'});
+    return res.status(401).json({error:'invalid token'});
 
   logger.error(error.message);
 
