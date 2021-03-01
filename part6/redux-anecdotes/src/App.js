dc303e81409d6d0ca4856aff63025f4dc2397a7f
Andrea Fanti <andrea.fanti@aalto.fi>
2021-03-01 22:56:41 +0200

    ex. 6.5 done

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -19,16 +19,17 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
-        <div key={anecdote.id}>
-          <div>
-            {anecdote.content}
+      {anecdotes
+        .sort((a1, a2) => a2.votes-a1.votes)
+        .map(anecdote => <div key={anecdote.id}>
+            <div>
+              {anecdote.content}
+            </div>
+            <div>
+              has {anecdote.votes}
+              <button onClick={()=>vote(anecdote.id)}>vote</button>
+            </div>
           </div>
-          <div>
-            has {anecdote.votes}
-            <button onClick={()=>vote(anecdote.id)}>vote</button>
-          </div>
-        </div>
       )}
       <h2>create new</h2>
       <form onSubmit={handleCreateNewAnecdode}>
