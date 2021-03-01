ca57f87c4b75b4c58443d70b61e0135a605ec22c
Andrea Fanti <andrea.fanti@aalto.fi>
2021-03-01 02:06:00 +0200

    ex. 5.17 done

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -14,6 +14,7 @@ const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const testingRouter = require('./controllers/testing');
 
 const middleware = require('./utils/middleware');
 
@@ -34,6 +35,10 @@ app.use(morgan('tiny'));
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
+
+if (process.env.NODE_ENV === 'test') {
+  app.use('/api/testing', testingRouter);
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
